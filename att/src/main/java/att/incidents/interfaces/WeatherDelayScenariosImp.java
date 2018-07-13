55392cd242d28837afa8731c6a416debf6349578
Franco Frangipane <ffrangipane@amelco.co.uk>
2018-07-13 12:55:12 +0100

    Weather incidents merged in a single class for both IMG and Betradar

--- att/src/main/java/att/incidents/interfaces/WeatherDelayScenariosImp.java
+++ att/src/main/java/att/incidents/interfaces/WeatherDelayScenariosImp.java
@@ -1,12 +1,13 @@
 package att.incidents.interfaces;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ats.betting.trading.att.ws.scenario.dto.Incident;
 import ats.betting.trading.att.ws.scenario.dto.Side;
 import att.incidents.TennisIncidentsHelper;
 
-public abstract class WeatherDelayScenariosImp implements WeatherDelayScenarios{
+public class WeatherDelayScenariosImp implements WeatherDelayScenarios{
 
     protected List<Incident> incidents;
     protected TennisIncidentsHelper tennisIncidentsHelper;
@@ -103,6 +104,39 @@ public abstract class WeatherDelayScenariosImp implements WeatherDelayScenarios{
         incidents.add(tennisIncidentsHelper.getPointIncident("0:50", "40-0",
                 Side.HOME, Side.HOME, "0,0,0", "0,0,0", 1, 1));
 
+        return incidents;
+    }
+
+    @Override
+    public List<Incident> getMatchIncidents(TennisIncidentsHelper tennisIncidentsHelper) {
+        String initialTime = "0:00:00";
+        incidents = new ArrayList<>();
+
+        incidents.add(tennisIncidentsHelper.getPregameIncident());
+        incidents.add(tennisIncidentsHelper.getPregameTossIncidentIMG(Side.HOME)); //TODO remove?
+        incidents.add(tennisIncidentsHelper.getMatchStateChangedIncidentIMG()); //TODO would affect BR?
+        incidents.add(tennisIncidentsHelper.getServeFirstIncident(Side.HOME));
+
+        incidents.add(tennisIncidentsHelper.getPeriodIncident(initialTime, Side.HOME, 1, 1));
+
+        incidents.add(tennisIncidentsHelper.getPointIncident("0:10", "15-0",
+                Side.HOME, Side.HOME, "0,0,0", "0,0,0",
+                1, 1));
+        incidents.add(tennisIncidentsHelper.getPointIncident("0:20", "30-0",
+                Side.HOME, Side.HOME, "0,0,0", "0,0,0",
+                1, 1));
+
+        return incidents;
+    }
+
+    @Override
+    public List<Incident> getWeatherStopPlayIncidents() {
+        tennisIncidentsHelper = new TennisIncidentsHelper();
+        incidents = getMatchIncidents(tennisIncidentsHelper);
+
+        incidents.add(tennisIncidentsHelper.getWeatherStopPlayIncidentBetRadar("0:30", "30-0",
+                Side.HOME, "0,0,0", "0,0,0",
+                1, 1));
         return incidents;
     }
 
